11c894d2269cae943c4cd0215fd4e12f81e09372
Gideon Mensah <gomy1233@gmail.com>
2025-04-18 11:56:41 +0000

    create time function

--- Project_1/main.py
+++ Project_1/main.py
@@ -6,5 +6,8 @@ from time import strftime
 root = Tk()
 root.title("Clock")
 
+def time():
+    string = strftime("%H:%M:%S %p")
+
 label = Label(root, font=("ds-digital", 90), background='black', foreground="gold")
 label.pack(anchor="center")
