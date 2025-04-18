decf2b032cc18618b72d4120bc6505327d62b551
Gideon Mensah <gomy1233@gmail.com>
2025-04-18 12:50:24 +0000

    add function

--- Project_2/main.py
+++ Project_2/main.py
@@ -2,3 +2,12 @@
 # The import the package here
 
 import phonenumbers
+from test import number
+
+from phonenumbers import geocoder
+
+#below is to show phone number location
+ch_number = phonenumbers.parse(number, "CH")
+
+print(geocoder.description_for_number(ch_number, "en"))
+
