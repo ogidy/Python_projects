2c05d154ac73eaea739d569f9f575b4086838a81
Gideon Mensah <gomy1233@gmail.com>
2025-04-18 12:00:39 +0000

    add time config

--- Project_1/main.py
+++ Project_1/main.py
@@ -7,7 +7,8 @@ root = Tk()
 root.title("Clock")
 
 def time():
-    string = strftime("%H:%M:%S %p")
-
+    string = strftime("%H:%M:%S %p") #%H makes it call 24hr time whiles %I call 12hr time
+    label.config(text=string)
+    label.after(1000,time) #call time function after every 1000 millisecond
 label = Label(root, font=("ds-digital", 90), background='black', foreground="gold")
 label.pack(anchor="center")
